a11748cd865f568319c5930f947c6d4fb087934a
Md. Shakil <shakil.eub.cse@gmail.com>
2024-02-05 03:03:44 +0600

    Image Compress Code update

--- Python Helpful Program/8.Image Compress.py
+++ Python Helpful Program/8.Image Compress.py
@@ -1,9 +1,16 @@
 from PIL import Image
+import os
 
-def compress_image(input_path, output_path, quality=20):
+def compress_image(input_path, output_folder, quality=20):
     try:
         # Open an image file
         with Image.open(input_path) as img:
+            # Get the filename and extension from the input path
+            file_name, file_extension = os.path.splitext(os.path.basename(input_path))
+
+            # Compose the output path using the same filename and extension
+            output_path = os.path.join(output_folder, f"{file_name}_compressed{file_extension}")
+
             # Save the image with compression
             img.save(output_path, quality=quality)
             print(f"Image compressed successfully. Saved at: {output_path}")
@@ -12,5 +19,5 @@ def compress_image(input_path, output_path, quality=20):
 
 # Example usage
 input_image_path = "Pic.jpg"
-output_image_path = "output_compressed.jpg"
-compress_image(input_image_path, output_image_path)
+output_folder = "output_folder"
+compress_image(input_image_path, output_folder)
